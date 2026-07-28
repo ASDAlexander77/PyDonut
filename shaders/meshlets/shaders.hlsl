117a2a3fe241863210bcbdc5426f03a5e53d1811
ASDAlexander77 <duzhar@googlemail.com>
2026-07-29 00:00:41 +0100

    Refactor meshlet shader code to improve structure and variable naming

--- shaders/meshlets/shaders.hlsl
+++ shaders/meshlets/shaders.hlsl
@@ -1,40 +1,84 @@
-struct Payload
-{
-    uint dummy;
+/*
+* Copyright (c) 2014-2021, NVIDIA CORPORATION. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
+#define MAX_PRIMS_PER_MESHLET 1
+#define MAX_VERTICES_PER_MESHLET 3
+
+static const float2 g_positions[] = {
+	float2(-0.5, -0.5),
+	float2(0, 0.5),
+	float2(0.5, -0.5)
 };
 
-struct VertexOutput
+static const float3 g_colors[] = {
+	float3(1, 0, 0),
+	float3(0, 1, 0),
+	float3(0, 0, 1)	
+};
+
+struct Payload
 {
-    float4 position : SV_Position;
+	int dummy;
+};
+
+struct Vertex
+{
+	float4 pos : SV_Position;
+	float3 color : COLOR;
 };
 
 groupshared Payload s_payload;
 
 [numthreads(1, 1, 1)]
-void main_as(uint3 dispatchThreadID : SV_DispatchThreadID)
+void main_as(
+	uint globalIdx : SV_DispatchThreadID)
 {
-    s_payload.dummy = 0;
-
-    DispatchMesh(1, 1, 1, s_payload);
+	s_payload.dummy = 0;
+	DispatchMesh(1, 1, 1, s_payload);
 }
 
-[numthreads(1, 1, 1)]
+[numthreads(1,1,1)]
 [outputtopology("triangle")]
 void main_ms(
-    in payload Payload payload,
-    out vertices VertexOutput verts[3],
-    out indices uint3 tris[1])
+    uint threadId : SV_GroupThreadID,
+    in payload Payload i_payload,
+    out indices uint3 o_tris[MAX_PRIMS_PER_MESHLET],
+    out vertices Vertex o_verts[MAX_VERTICES_PER_MESHLET])
 {
     SetMeshOutputCounts(3, 1);
+    o_tris[0] = uint3(0, 1, 2);
 
-    verts[0].position = float4( 0.0f, -0.5f, 0.0f, 1.0f);
-    verts[1].position = float4( 0.5f,  0.5f, 0.0f, 1.0f);
-    verts[2].position = float4(-0.5f,  0.5f, 0.0f, 1.0f);
-
-    tris[0] = uint3(0, 1, 2);
+    for (uint i = 0; i < 3; i++)
+    {
+    	o_verts[i].pos = float4(g_positions[i], 0, 1);
+    	o_verts[i].color = g_colors[i];
+    }
 }
 
-float4 main_ps(VertexOutput input) : SV_Target0
+
+void main_ps(
+	in Vertex i_vertex,
+	out float4 o_color : SV_Target0
+)
 {
-    return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	o_color = float4(i_vertex.color, 1);
 }
